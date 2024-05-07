6fe2364e791c1a67b7a258107eadf4c5b7654842
DimensionNightmare <dimensionnightmare@outlook.com>
2024-05-07 11:26:10 +0800

    up

--- Source/Afx/DbAfx.h
+++ Source/Afx/DbAfx.h
@@ -2,11 +2,13 @@
 
 import DNDbObj;
 
-#define DBSelect(obj, name) .Select(#name, [&obj](){ return obj.name(); })
-#define DBSelectCond(obj, name, cond, splicing) .SelectCond(obj, #name, cond, splicing, [&obj](){ return obj.name(); })
-#define DBUpdate(obj, name) .Update(obj, #name, obj.name())
-#define DBUpdateCond(obj, name, cond, splicing) .UpdateCond(obj, #name, cond, splicing, [&obj](){ return obj.name(); })
-#define DBDeleteCond(obj, name, cond, splicing) .DeleteCond(obj, #name, cond, splicing, [&obj](){ return obj.name(); })
+// obj.name is to verify that the name is a member of the obj
+// lambda has no practical meaning 
+#define DBSelect(obj, name) .Select(#name, [&](){return obj.name();})
+#define DBSelectCond(obj, name, cond, splicing) .SelectCond(obj, #name, cond, splicing, [&](){return obj.name();})
+#define DBUpdate(obj, name) .Update(obj, #name, [&](){return obj.name();})
+#define DBUpdateCond(obj, name, cond, splicing) .UpdateCond(obj, #name, cond, splicing, [&](){return obj.name();})
+#define DBDeleteCond(obj, name, cond, splicing) .DeleteCond(obj, #name, cond, splicing, [&](){return obj.name();})
 
 
 // template
